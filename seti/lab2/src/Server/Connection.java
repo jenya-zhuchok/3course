c99e11a005cb6089a0fd543404aaec7c07054724
AlisiaSmith <anya0192837465@ya.ru>
2020-11-27 22:55:31 +0700

    я буду долго впоминать эти муки ада

--- seti/lab2/src/Server/Connection.java
+++ seti/lab2/src/Server/Connection.java
@@ -8,62 +8,74 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class Connection implements Runnable{
-    private Socket clientSocket;
-    private long numReceivedBytes;
 
-    public Connection(Socket clientSocket){
-        this.clientSocket = clientSocket;
-    }
+    private Socket clientSocket;
     private FileOutputStream fileOut;
     private DataOutputStream writeInSocket;
     private DataInputStream readFromSocket;
 
-    @Override public void run(){
-        try {
 
-            int nameSize, part;
-            long fileSize, t1, t2, t3 = 0;
-            float dif;
-            String fileName;
-            byte[] arrayNameBytes;
-            byte[] arrayFileBytes = new byte[4096];
+    public Connection(Socket clientSocket) throws IOException {
+        this.clientSocket = clientSocket;
+        writeInSocket = new DataOutputStream(clientSocket.getOutputStream()); //канал записи в сокет
+        readFromSocket = new DataInputStream(clientSocket.getInputStream()); //канал чтения из сокета
+    }
 
-            //start new thread with accepted socket
-            System.out.print("Have connection!\n");
+    private String getFileName() throws IOException {
+        int nameSize = readFromSocket.readInt();
+        byte[] arrayNameBytes = new byte[nameSize];
+        readFromSocket.readFully(arrayNameBytes, 0, nameSize);
+        return new String(arrayNameBytes, StandardCharsets.UTF_8);
+    }
 
-            writeInSocket = new DataOutputStream(clientSocket.getOutputStream()); //канал записи в сокет
-            readFromSocket = new DataInputStream(clientSocket.getInputStream()); //канал чтения из сокета
 
-            nameSize = readFromSocket.readInt();
-            arrayNameBytes = new byte[nameSize];
-            readFromSocket.readFully(arrayNameBytes, 0, nameSize);
-            fileName = new String(arrayNameBytes, StandardCharsets.UTF_8);
-            fileOut = new FileOutputStream("D:/repos/git/3_corse/3course/seti/lab2/" + fileName);
-            fileSize = readFromSocket.readLong();
-            long read = 0;
-            t1 = System.currentTimeMillis();
-            t2 = t1;
+    private double spead(long size, long time){ return size/(time*1000); }
 
-            while (read < fileSize) {
-                part = readFromSocket.read(arrayFileBytes);
-                t3 = System.currentTimeMillis();
-                dif = t3 - t2;
-                if (dif >= 3000) {
-                    System.out.printf("Instant speed = %f Mb/sec\n",part/(dif*1000));
-                    t2 = t3;
-                }
-                fileOut.write(arrayFileBytes, 0, part);
-                read += part;
+    private long processing(String fileName, long fileSize) throws IOException {
+        byte[] arrayFileBytes = new byte[4096];
+        long t1, t2, diftime;
+        long read = 0;
+        int part, difpart = 0;
+        t1 = System.currentTimeMillis();
+
+        while (read < fileSize) {
+            part = readFromSocket.read(arrayFileBytes);
+            difpart += part;
+            t2 = System.currentTimeMillis();
+            diftime = t2 - t1;
+            if (diftime >= 3000) {
+                System.out.println(fileName + ": Instant speed = " + spead(difpart, diftime) + "Mb/sec");
+                t1 = t2;
+                difpart = 0;
             }
+            fileOut.write(arrayFileBytes, 0, part);
+            read += part;
+        }
+        return read;
+    }
 
-            dif = t3 - t1;
-            //if (dif < 3000) {
-            //  System.out.printf("Instant speed = %f Mb/sec\n",read/(dif*1000));
-            //}
-            System.out.printf("Speed per session = %f Mb/sec\n", read/(dif*1000));
-            numReceivedBytes = read;
-            if(read == fileSize) System.out.print("Size of received file is equal size of sended file\n");
-            else System.out.print("Size of received file is not equal size of sended file\n");
+
+    @Override
+    public void run(){
+        long fileSize, t1, t2 = 0;
+        String fileName;
+
+        //start new thread with accepted socket
+        System.out.print("Have connection!\n");
+
+        try {
+            fileName = getFileName();
+            fileSize = readFromSocket.readLong();
+
+            fileOut = new FileOutputStream("D:/repos/git/3_corse/3course/seti/lab2/uploads/" + fileName);
+
+            t1 = System.currentTimeMillis();
+            long read = processing(fileName, fileSize);
+            t2 = System.currentTimeMillis();
+
+            System.out.println(fileName + ": Speed per session = " + spead(read,t2 - t1 ) + " Mb/sec");
+            if(read == fileSize) System.out.println(fileName + ": Size of received file is equal size of sended file");
+            else System.out.println(fileName + ": Size of received file is not equal size of sended file");
 
         } catch (IOException e) {
             e.printStackTrace();
