6b0d2b783682547c18c777a72ad96147381d848a
AlisiaSmith <anya0192837465@ya.ru>
2020-12-11 16:00:37 +0700

    -

--- v_mat/project3/src/Integral.java
+++ v_mat/project3/src/Integral.java
@@ -17,37 +17,54 @@ public class Integral {
         else return -(Math.cos(b) - Math.cos(a));
     }
 
+
+    private double inaccuracy(){
+        if(f == func.exp) return Math.exp(b)* Math.pow(b - a, 3) / (12 * Math.pow(n, 2));
+        else return Math.abs(Math.sin(Math.PI/2))/ (12 * Math.pow(n, 2));
+    }
+
     private double trapeze()
     {
-        double h = (a + b) / n;
-        double rez = 0;
+        double h = (a + b) / (n * 3);
+        double res = 0;
         double x0 = a;
         double x1= a + h;
 
         while (x0 < b){
-            rez += h * (calc(x0) + calc(x1)) / 2;
+            res += h * (calc(x0) + calc(x1)) / 2;
             x0 = x1;
             x1 += h;
         }
 
-        return rez;
+        return res;
     }
 
-    Integral(int i, int n){
-        if(i == 1){
+    Integral(func f, int n){
+        this.f = f;
+        if(f == func.exp){
             this.a = 0;
             this.b = 2;
-            f = func.exp;
         }
         else {
             this.a = 0;
             this.b = Math.PI;
-            f = func.sin;
         }
         this.n = n;
 
-        System.out.println("Must be: " + calcF());
-        System.out.println("Result: " + trapeze());
+        double must = calcF();
+        double res = trapeze();
+        double inac = inaccuracy();
+        double diff = Math.abs(res - must);
+
+        double m = must - inac;
+
+        System.out.println("Must be: " + must);
+        System.out.println("Result: " + res);
+        System.out.println("Inaccuracy: " + inac);
+        System.out.println("Difference: " + diff);
+
+        System.out.println("??: " +Math.abs(diff - inac));
+
 
     }
 
--- v_mat/project3/src/Main.java
+++ v_mat/project3/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args){
-        Integral i = new Integral(1, 1000);
+        Integral i = new Integral(Integral.func.sin, 20);
     }
 }
